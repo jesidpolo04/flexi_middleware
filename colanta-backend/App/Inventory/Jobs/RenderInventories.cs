0dd8690dfa70a2d687605be8a4e4070f64d7cf58
Jesid Polo <jesing482@gmail.com>
2023-10-20 21:46:35 -0500

    adding page whille to render inventories

--- colanta-backend/App/Inventory/Jobs/RenderInventories.cs
+++ colanta-backend/App/Inventory/Jobs/RenderInventories.cs
@@ -52,72 +52,85 @@
 
             Parallel.ForEach(allWarehouses, warehouse =>
             {
-                InventoriesRepository inventoriesLocalRepository = (InventoriesRepository) this.serviceProvider.GetService(typeof(InventoriesRepository));
+                InventoriesRepository inventoriesLocalRepository = (InventoriesRepository)this.serviceProvider.GetService(typeof(InventoriesRepository));
                 InventoriesVtexRepository inventoriesVtexRepository = (InventoriesVtexRepository)this.serviceProvider.GetService(typeof(InventoriesVtexRepository));
                 InventoriesSiesaRepository inventoriesSiesaRepository = (InventoriesSiesaRepository)this.serviceProvider.GetService(typeof(InventoriesSiesaRepository));
                 SkusRepository skusLocalRepository = (SkusRepository)this.serviceProvider.GetService(typeof(SkusRepository));
-                ILogger logger = (ILogger) this.serviceProvider.GetService(typeof(ILogger));
+                ILogger logger = (ILogger)this.serviceProvider.GetService(typeof(ILogger));
 
                 try
                 {
-                    Inventory[] siesaInventories = inventoriesSiesaRepository.getAllInventoriesByWarehouse(warehouse.siesa_id, 1).Result;
-                    this.obtainedInventories += siesaInventories.Length;
-                    foreach (Inventory siesaInventory in siesaInventories)
+                    int page = 1;
+                    while (true)
                     {
                         try
                         {
-                            if (!this.skuExists(siesaInventory, skusLocalRepository))
-                            {
-                                this.notProccecedInventories.Add(siesaInventory);
-                                continue;
-                            }
-                            Inventory localInventory = inventoriesLocalRepository.getInventoryBySkuErpIdAndWarehouseSiesaId(siesaInventory.sku_erp_id, siesaInventory.warehouse_siesa_id).Result;
-
-                            if (localInventory != null)
-                            {
-                                if (localInventory.quantity != siesaInventory.quantity)
-                                {
-                                    localInventory.quantity = siesaInventory.quantity;
-                                    localInventory = inventoriesLocalRepository.updateInventory(localInventory).Result;
-                                    inventoriesVtexRepository.updateInventory(localInventory);
-                                    inventoriesVtexRepository.removeReservedInventory(localInventory);
-                                    this.updatedInventories.Add(localInventory);
-                                }
-
-                                if (localInventory.quantity == siesaInventory.quantity)
-                                {
-                                    inventoriesVtexRepository.updateInventory(localInventory);
-                                    inventoriesVtexRepository.removeReservedInventory(localInventory);
-                                    this.notProccecedInventories.Add(localInventory);
-                                }
-                            }
-                            if (localInventory == null)
+                            Inventory[] siesaInventories = inventoriesSiesaRepository.getAllInventoriesByWarehouse(warehouse.siesa_id, page).Result;
+                            this.obtainedInventories += siesaInventories.Length;
+                            foreach (Inventory siesaInventory in siesaInventories)
                             {
                                 try
                                 {
-                                    localInventory = inventoriesLocalRepository.saveInventory(siesaInventory).Result;
+                                    if (!this.skuExists(siesaInventory, skusLocalRepository))
+                                    {
+                                        this.notProccecedInventories.Add(siesaInventory);
+                                        continue;
+                                    }
+                                    Inventory localInventory = inventoriesLocalRepository.getInventoryBySkuErpIdAndWarehouseSiesaId(siesaInventory.sku_erp_id, siesaInventory.warehouse_siesa_id).Result;
+
+                                    if (localInventory != null)
+                                    {
+                                        if (localInventory.quantity != siesaInventory.quantity)
+                                        {
+                                            localInventory.quantity = siesaInventory.quantity;
+                                            localInventory = inventoriesLocalRepository.updateInventory(localInventory).Result;
+                                            inventoriesVtexRepository.updateInventory(localInventory);
+                                            inventoriesVtexRepository.removeReservedInventory(localInventory);
+                                            this.updatedInventories.Add(localInventory);
+                                        }
+
+                                        if (localInventory.quantity == siesaInventory.quantity)
+                                        {
+                                            inventoriesVtexRepository.updateInventory(localInventory);
+                                            inventoriesVtexRepository.removeReservedInventory(localInventory);
+                                            this.notProccecedInventories.Add(localInventory);
+                                        }
+                                    }
+                                    if (localInventory == null)
+                                    {
+                                        try
+                                        {
+                                            localInventory = inventoriesLocalRepository.saveInventory(siesaInventory).Result;
+                                        }
+                                        catch (Exception exception)
+                                        {
+                                            logger.writelog(new Exception($"No se pudo guardar el inventario {localInventory.ToString()} en la tienda ${localInventory.warehouse_siesa_id}"));
+                                            continue;
+                                        }
+                                        inventoriesVtexRepository.updateInventory(localInventory);
+                                        this.loadInventories.Add(localInventory);
+                                    }
+                                }
+                                catch (VtexException vtexException)
+                                {
+                                    this.failedInventories.Add(siesaInventory);
+                                    this.console.throwException(vtexException.Message);
+                                    logger.writelog(vtexException);
                                 }
                                 catch (Exception exception)
                                 {
-                                    logger.writelog(new Exception($"No se pudo guardar el inventario {localInventory.ToString()} en la tienda ${localInventory.warehouse_siesa_id}"));
-                                    continue;
+                                    this.failedInventories.Add(siesaInventory);
+                                    this.console.throwException(exception.Message);
+                                    logger.writelog(exception);
                                 }
-                                inventoriesVtexRepository.updateInventory(localInventory);
-                                this.loadInventories.Add(localInventory);
                             }
                         }
-                        catch (VtexException vtexException)
+                        catch (Exception e)
                         {
-                            this.failedInventories.Add(siesaInventory);
-                            this.console.throwException(vtexException.Message);
-                            logger.writelog(vtexException);
+                            this.console.throwException(e.Message);
+                            break;
                         }
-                        catch (Exception exception)
-                        {
-                            this.failedInventories.Add(siesaInventory);
-                            this.console.throwException(exception.Message);
-                            logger.writelog(exception);
-                        }
+                        page ++;
                     }
                 }
                 catch (SiesaException siesaException)
@@ -138,7 +151,7 @@
         private bool skuExists(Inventory inventory, SkusRepository repository)
         {
             Task<Sku> sku = repository.getSkuBySiesaId(inventory.sku_erp_id);
-            if(sku.Result == null)
+            if (sku.Result == null)
             {
                 return false;
             }
